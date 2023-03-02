ccb9a1913dc9eb74ffe58f9fb55b01fae7a0763b
Rafael Henrique da  Silva <adm.fael.hs@gmail.com>
2023-03-02 16:36:48 -0300

    Create XboxRF.ino

---
+++ XboxRF.ino
@@ -0,0 +1,56 @@
+int SYNC = 2;
+int RT = A3;
+bool active = false;
+bool oldOn = false;
+
+void setup() {
+  pinMode(SYNC, INPUT);
+  pinMode(MOTOR, OUTPUT);
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  if (digitalRead(SYNC) == LOW) {
+    if (oldOn == false) {
+      if (active == false) {
+        active = true;
+      } else {
+        active = false;
+        for (int i = 0; i < 3; i++) {
+        }
+      }
+      oldOn = true;
+      delay(500);
+    }
+  } else {
+    oldOn = false;
+  }
+
+
+  pinMode(RT, INPUT);
+  int sig = analogRead(RT);
+
+
+  // THIS SHOULD FIX THE RANDOM SHOTS WHILE THE MOD IS ON
+  if (active == true) {
+    pinMode(RT, OUTPUT);
+    analogWrite(RT, 1000);
+    delay(22);
+    pinMode(RT, INPUT);
+    delay(100);
+  }
+
+
+
+  if (active == true) {
+    int chicken = 4;
+  }
+
+
+  else if (active == false) {
+    pinMode(RT, INPUT);
+  } else if (sig > 150 || sig == 0 && active == false) {
+    pinMode(RT, INPUT);
+  }
+}
